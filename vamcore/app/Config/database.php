1f7602f1cdb629ca76d607997e1b80c56fe7c5f6
Elsalvaje63 <ginartealdo@gmail.com>
2026-04-26 09:15:18 -0400

    Add files via upload

---
+++ vamcore/app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Postgres',
+		'persistent' => false,
+		'host' => 'dpg-d7n04c1kh4rs73aue28g-a',
+		'port' => 5432,
+		'login' => 'admin',
+		'password' => 'H9PbVzWWtoqAyhOXo2y0z277NXERZTSK',
+		'database' => 'aerocuba',
+		'encoding' => 'utf8'
+	);
+}
